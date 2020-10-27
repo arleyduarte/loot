94cfd921b60412195780d792bed92829b483846b
Arley Duarte <arleymauricio@gmail.com>
2020-10-27 14:38:22 -0300

    react test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ const App = () => {
       <hr />
       <Wallet />
       <Loot />
+
+      <div>
+        Powered by{" "}
+        <a target="_blank" href="https://www.coindesk.com/price">
+          {" "}
+          Coindesk
+        </a>
+      </div>
     </div>
   );
 };
